50e9643eaddc2870394749ef017afc35c4d1b320
HynekSvoboda <97514770+HynekSvoboda@users.noreply.github.com>
2026-04-14 19:14:39 +0200

    a-h

--- src/projekt/DatovyAnalytik.java
+++ src/projekt/DatovyAnalytik.java
@@ -5,7 +5,7 @@ public class DatovyAnalytik extends Zamestnanec {
     public DatovyAnalytik(int ID, String jmeno, String prijmeni, int rokNarozeni) {
         super(ID, jmeno, prijmeni, rokNarozeni);
     }
-
+ 
     @Override
     public void spustitDovednost() {
         Map<Zamestnanec, UrovenSpoluprace> mojiSpolupracovnici = getSpolupracovnici();
